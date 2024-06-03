30615a9fc0c470b4d414c4f91f82eb8970571d3f
mohammodjuwelislam <mohammodjuwelislam@gmail.com>
2024-06-04 02:47:27 +0600

    search bar complete

--- js/main.js
+++ js/main.js
@@ -1,38 +1,16 @@
-$(document).ready(function(){
-
-
-    // header sticky 
-    $(window).scroll(function(){
-        var sticky = $('.sticky'),
-            scroll = $(window).scrollTop();
-      
-        if (scroll >= 100) sticky.addClass('fixed');
-        else sticky.removeClass('fixed');
-    });
-
-
-    // testimonial slider 
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        navText: ['<img src="assets/icon/left_arrow.png" alt="left">', '<img src="assets/icon/right_arrow.png" alt="right"></img>'],
-        nav:true,
-        dots: true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:1
-            },
-            1000:{
-                items:1
-            }
-        }
-    });
-
-});
-
+const search_btn = document.querySelector('.search_show_btn');
+const close_btn = document.querySelector('.close_btn');
+const overlay_btn = document.querySelector('.overlay');
+const search_bar = document.querySelector('.search_bar_section');
+search_btn.addEventListener('click', () => {
+    search_bar.classList.toggle('active')
+})
+close_btn.addEventListener('click', () => {
+    search_bar.classList.toggle('active')
+})
+overlay_btn.addEventListener('click', () => {
+    search_bar.classList.toggle('active')
+})
 
     
 
